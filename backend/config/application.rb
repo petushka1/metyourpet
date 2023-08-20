20103d98689a56b016962e47b42fc02a94b0587d
petushka1 <nadya@devery.io>
2023-08-20 16:58:59 +0300

    fix bundle issue

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,9 +8,18 @@ module Backend
   class Application < Rails::Application
     config.load_defaults 7.0 if defined?(config.load_defaults)
     config.public_file_server.enabled = true
+    
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=31536000',
+      'Expires' => 1.year.from_now.to_formatted_s(:rfc822)
+    }
+
+    config.public_file_server.static_paths || = []    
+    config.public_file_server.static_paths << Rails.root.join('frontend', 'dist')
+    
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001' # Replace with your frontend domain or origin
+        origins 'http://localhost:3001'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # XUMM sign-in routes
 
+  # Route to serve static assets
+  # get '/dist/bundle.js', to: 'static#serve_index_js'
+
+  # XUMM sign-in routes
   get '/create_login', to: 'xumm#create_login'
   post '/xumm_callback', to: 'xumm#callback'
 
-   # Route to serve static assets
-   get '/bundle.js', to: 'static#serve_index_js'
 end
